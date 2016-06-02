b2bf08bba2bdf5aa73abb0eccfd2d51fcd4b036b
Bigbigboom <39602810@qq.com>
2016-06-02 09:37:02 +0800

    update

---
+++ seckill/src/test/java/org/hh/seckill/dao/IBaseDaoTest.java
@@ -0,0 +1,24 @@
+package org.hh.seckill.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IBaseDaoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
